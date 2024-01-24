ea0ca2e1d26fb1a9c9eeb7d75815c23824e8f192
JoseManuelYC <joseyanezcontact@gmail.com>
2024-01-24 13:19:11 -0400

    first commit

---
+++ python.py
@@ -0,0 +1,27 @@
+#Hola Mundo
+print("Hola mundo")
+
+#Variables
+
+my_string = 'Esto es una cadena de texto'
+my_string = "Mutando cadena de texto"
+print(type(my_string))
+
+my_string= 6 #Tipado dinamico
+print(type(my_string))
+
+my_int = 5
+my_int = my_int + 5
+my_float= 2.5
+print(my_int + my_float)
+
+my_bool = True
+my_false = False
+
+#Concatenar
+print("El valor de mi entero es "+ str(my_int) + " Y el de mi bool es " + str(my_bool))
+print(f"El valor de mi entero es {my_int} Y el de mi bool es {my_bool}")
+
+#Listas
+
+my_list = [my_int, my_string, my_bool]
